e36c0793bbab9b2d946f3e2bdc94012ed572318f
Jairo <jairo.jb@gmail.com>
2021-04-13 21:08:44 -0300

    OrderItem, many-to-many association with extra attributes PART 2

---
+++ src/main/java/com/jairo/course/repositories/OrderItemRepository.java
@@ -0,0 +1,11 @@
+package com.jairo.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jairo.course.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+	
+	
+
+}
